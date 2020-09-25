60fcb5acda01ad9e0213a99f33d7f2968d4d291b
bugparty <fancycode@gmail.com>
2020-09-25 19:48:28 +0800

    reformat AddTodo.js VisibleTodoList.js

--- src/componments/todo/containers/VisibleTodoList.js
+++ src/componments/todo/containers/VisibleTodoList.js
@@ -1,7 +1,7 @@
-import { connect } from 'react-redux'
-import {toggleTodo, deleteTodo, deleteSubTodo, toggleSubTodo, addSubTodo} from "../todoSlice"
+import {connect} from 'react-redux'
+import {addSubTodo, deleteSubTodo, deleteTodo, toggleSubTodo, toggleTodo} from "../todoSlice"
 import TodoList from "../TodoList"
-import { VisibilityFilters} from "../visibilityFilterSlice";
+import {VisibilityFilters} from "../visibilityFilterSlice";
 
 const getVisibleTodos = (todos, filter) => {
     switch (filter) {
