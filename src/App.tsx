8ae6516126d75038606fe2f92fa98d0c43e138bd
fancy <fancycode@gmail.com>
2021-08-30 01:51:51 +0800

    refactor the whole program to use redux, remove reduplicated code

--- src/App.tsx
+++ src/App.tsx
@@ -38,71 +38,12 @@ class App extends Component<AppProps, AppState> {
     const tickingSound = isExist("enableTickingSound") || true;
     const restTickingSound = isExist("enableRestTickingSound") || false;
 
-    this.state = {
-      status: "reset",
-      mode: "pomodoro",
-      enableTickingSound: tickingSound,
-      enableRestTickingSound: restTickingSound,
-      pomodoro_duration: 25 * 60,
-      short_break_duration: 5 * 60,
-      long_break_duration: 15 * 60,
-    };
-
-    this.setBegin = this.setBegin.bind(this);
-    this.setEnd = this.setEnd.bind(this);
-    this.setReset = this.setReset.bind(this);
-    this.setTickingSound = this.setTickingSound.bind(this);
-    this.saveOptions = this.saveOptions.bind(this);
-    this.setRestTickingSound = this.setRestTickingSound.bind(this);
-    this.setMode = this.setMode.bind(this);
   }
-
-  setBegin() {
-    this.setState({ status: "begin" });
-  }
-
-  setEnd() {
-    this.setState({ status: "end" });
-  }
-
-  setReset() {
-    this.setState({ status: "reset" });
-  }
-
-  setMode(mode: AppMode) {
-    this.setState({ mode: mode });
-  }
-
-  setTickingSound(isEnable: boolean) {
-    this.setState({ enableTickingSound: isEnable }, () => {
-      localStorage.setItem("enableTickingSound", isEnable.toString());
-    });
-  }
-
-  setRestTickingSound(isEnable: boolean) {
-    this.setState({ enableRestTickingSound: isEnable }, () => {
-      localStorage.setItem("enableRestTickingSound", isEnable.toString());
-    });
-  }
-
-  saveOptions(options: Options) {
-    this.setState(options);
-  }
-
   render() {
     return (
       <div className="App">
         <div className="AppWrapper">
-          <Navbar
-            enableTickingSound={this.state.enableTickingSound}
-            enableRestTickingSound={this.state.enableRestTickingSound}
-            setTickingSound={this.setTickingSound}
-            setRestTickingSound={this.setRestTickingSound}
-            pomodoro_duration={this.state.pomodoro_duration}
-            short_break_duration={this.state.short_break_duration}
-            long_break_duration={this.state.long_break_duration}
-            saveOptions={this.saveOptions}
-          />
+          <Navbar/>
           <div className="columns">
             <div className="TodoContainer column is-one-quarter">
               <h2 className="h2">Todo list</h2>
@@ -112,15 +53,7 @@ class App extends Component<AppProps, AppState> {
               <VisibleTodoList />
             </div>
             <div className="ClockContainer column">
-              <ClockController
-                setBegin={this.setBegin}
-                setEnd={this.setEnd}
-                setReset={this.setReset}
-                pomodoro_duration={this.state.pomodoro_duration}
-                short_break_duration={this.state.short_break_duration}
-                long_break_duration={this.state.long_break_duration}
-                setMode={this.setMode}
-              />
+              <ClockController/>
               <Introduce />
             </div>
             <div className="OpLogContainer column is-one-quarter">
--- src/componments/clock/ClockController.js
+++ src/componments/clock/ClockController.js
@@ -4,59 +4,6 @@ import ClockButtons from "./ClockButtons2";
 import ClockTimer from "./ClockTimer";
 
 export class ClockController extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { timeInterval: 25 * 60, timeLeft: 25 * 60, timer: null };
-    this.timeBegin = this.timeBegin.bind(this);
-    this.timeReset = this.timeReset.bind(this);
-    this.setDuration = this.setDuration.bind(this);
-    this.handleLong = this.handleLong.bind(this);
-    this.handleLongReset = this.handleLongReset.bind(this);
-    this.handleShortReset = this.handleShortReset.bind(this);
-  }
-
-  timeBegin() {
-    let that = this;
-    let timer = setInterval(() => {
-      if (that.state.timeLeft <= 0) {
-        clearInterval(that.state.timer);
-        that.setState({ timeLeft: 0 });
-        that.props.setEnd();
-      } else {
-        that.setState({ timeLeft: that.state.timeLeft - 1 });
-      }
-    }, 1000);
-    this.setState({ timer: timer });
-    this.props.setBegin();
-  }
-
-  setDuration(seconds) {
-    clearInterval(this.state.timer);
-    this.setState({ timeInterval: seconds, timeLeft: seconds });
-  }
-
-  timeReset() {
-    clearInterval(this.state.timer);
-    this.setState({ timeLeft: this.state.timeInterval });
-    this.props.setReset();
-  }
-
-  handleLong() {
-    this.props.setMode("pomodoro");
-    this.setDuration(this.props.pomodoro_duration);
-    this.timeBegin();
-  }
-  handleLongReset() {
-    this.props.setMode("longRest");
-    this.setDuration(this.props.long_break_duration);
-    this.timeBegin();
-  }
-
-  handleShortReset() {
-    this.props.setMode("shortRest");
-    this.setDuration(this.props.short_break_duration);
-    this.timeBegin();
-  }
   render() {
     return (
       <div>
