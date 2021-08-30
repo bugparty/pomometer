8329ead7d33dd4a6ea8d40017e195c730f43ca79
fancy <fancycode@gmail.com>
2021-08-30 13:33:47 +0800

    fix ci

--- src/componments/todo/opSlice.js
+++ src/componments/todo/opSlice.js
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
-
+export const Op = {
+  start_short_rest: "start_short_rest",
+  start_long_rest: "start_long_rest",
+  start_pomodoro: "start_pomodoro",
+  reset_timer: "reset_timer",
+};
 export const opSlice = createSlice({
   name: "oplogs",
   initialState: [],
@@ -9,18 +14,22 @@ export const opSlice = createSlice({
       reducer(state, action) {
         state.push({
           id: action.payload.id,
+          op: action.payload.op,
           text: action.payload.text,
           todoId: action.payload.todoId,
+          subTodoId: action.payload.subTodoId,
           duration: action.payload.duration,
           createdDate: action.payload.createdDate,
         });
       },
-      prepare(todoId, text, duration) {
+      prepare(todoId, subTodoId, op, text, duration) {
         return {
           payload: {
             id: uuidv4(),
+            op: op,
             text: text,
             todoId: todoId,
+            subTodoId: subTodoId,
             duration: duration,
             createdDate: new Date().toJSON(),
           },
