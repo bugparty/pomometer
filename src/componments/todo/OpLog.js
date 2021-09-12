9ae15e521ae52a96fe7b1df12fbabaf5aae70dc4
fancy <fancycode@gmail.com>
2021-09-13 00:12:39 +0800

    add scrollbar to op log

--- src/componments/todo/OpLog.js
+++ src/componments/todo/OpLog.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { List, Typography } from "antd";
-import {ClockCircleOutlined}from '@ant-design/icons';
+import {List, Typography} from "antd";
+import {ClockCircleOutlined} from '@ant-design/icons';
+import InfiniteScroll from 'react-infinite-scroller'
+
 class OpLog extends React.Component {
-  render() {
-    return (
-      <List
-        header={<div></div>}
-        footer={<div></div>}
-        bordered
-        dataSource={this.props.opLogs}
-        renderItem={(item) => (
-          <List.Item>
-            <Typography.Text mark><ClockCircleOutlined /></Typography.Text> {item}
-          </List.Item>
-        )}
-      />
-    );
-  }
+    handleInfiniteOnLoad = () => {
+    };
+
+    render() {
+        return (
+            <InfiniteScroll className={"oplog_list"}
+                            pageStart={0}
+                            hasMore={false}
+                            loadMore={this.handleInfiniteOnLoad}
+                            useWindow={false}>
+                <List
+                    header={<div></div>}
+                    footer={<div></div>}
+                    bordered
+                    dataSource={this.props.opLogs}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Typography.Text mark><ClockCircleOutlined/></Typography.Text> {item}
+                        </List.Item>
+                    )}
+                />
+            </InfiniteScroll>
+
+        );
+    }
 }
+
 export default OpLog;
